e2107735e5aef967adc0de9cfcecdb84b639af27
kimhyeji <gpwl5589@gmail.com>
2025-09-23 16:26:01 +0900

    security

--- src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
@@ -1,8 +1,12 @@
 package com.example.demo.domain.member.controller;
 
+import com.example.demo.domain.member.request.MemberRequest;
 import com.example.demo.domain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "ApiV1MemberController", description = "회원 인증/인가 API")
 public class ApiV1MemberController {
     private final MemberService memberService;
+    
+    @PostMapping("/join")
+    public String join (@Valid @RequestBody MemberRequest memberRequest) {
+        memberService.join(memberRequest.getUsername(), memberRequest.getPassword());
+        
+        return "회원가입 성공";
+    }
 }
