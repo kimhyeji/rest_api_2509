186c830da47fb163c82774925e2c4beda4b84aa8
kimhyeji <gpwl5589@gmail.com>
2025-09-23 16:09:54 +0900

    add member

--- src/main/java/com/example/demo/domain/article/dto/ArticleDTO.java
+++ src/main/java/com/example/demo/domain/article/dto/ArticleDTO.java
@@ -10,6 +10,7 @@ public class ArticleDTO {
     private final Long id;
     private final String subject;
     private final String content;
+    private final String author;
     private final LocalDateTime createdDate;
     private final LocalDateTime modifiedDate;
 
@@ -17,6 +18,7 @@ public class ArticleDTO {
         this.id = article.getId();
         this.subject = article.getSubject();
         this.content = article.getContent();
+        this.author = article.getMember().getUsername();
         this.createdDate = article.getCreatedDate();
         this.modifiedDate = article.getModifiedDate();
 
