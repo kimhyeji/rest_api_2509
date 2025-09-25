58cb00fe9fa45ac4c89fb17512cba57448ff0de6
kimhyeji <gpwl5589@gmail.com>
2025-09-25 15:16:37 +0900

    jwt filter

--- src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
@@ -44,7 +44,6 @@ public class ApiV1MemberController {
         cookie.setMaxAge(60 * 60);
         res.addCookie(cookie);
 
-
         return RsData.of("200", "토큰 발급 성공 : " + accessToken, new MemberResponse(member));
     }
 
