47317251073b5ef47d649af22772c379c02dac41
kimhyeji <gpwl5589@gmail.com>
2025-09-26 17:20:06 +0900

    react nav

--- frontapp/src/App.jsx
+++ frontapp/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
 import Login from './pages/Login'
 import ArticleList from './pages/ArticleList'
+import Nav from './components/Nav'
 
 function App() {
     return (
         <>
             <BrowserRouter>
+                <Nav />
                 <Routes>
                     <Route index element={<Main />}></Route>
                     <Route path="/auth/login" element={<Login />} />
